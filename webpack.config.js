387b5c23631c6029c8029eb1de27b6d7bedc151c
Huan Li <nauhil.web@gmail.com>
2019-11-06 12:12:24 +0800

    add tsconfig webpack config and basic tsx files, you can npm start to view the app

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: '/node_modules/',
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin(),
+  ],
+};
